ee76b8bfa3d23e55ad6982e7126c547a06b2ef68
testing-10 <aj300803@gmail.com>
2025-03-01 11:25:55 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from app.routes import main
+import os
 
-app = Flask(__name__)
+# Get the absolute path to the template folder
+template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
+static_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'static'))
+
+# Create the Flask app with specified template and static folder
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 app.register_blueprint(main)
 
 # Import other components if needed
