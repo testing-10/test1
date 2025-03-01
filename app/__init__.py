a625f808e761916c294849fc3efab96397754d95
testing-10 <aj300803@gmail.com>
2025-03-01 10:40:37 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,8 @@
+from flask import Flask
+from app.routes import main
 
+app = Flask(__name__)
+app.register_blueprint(main)
+
+# Import other components if needed
+from app import resume_evaluator, utils
